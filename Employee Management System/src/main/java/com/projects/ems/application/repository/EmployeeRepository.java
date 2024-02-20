3cc8df04acabd91665f097f8ce9f358edc34fe50
Giridharan Sugunaseelan <144659110+SGN31@users.noreply.github.com>
2024-02-20 23:25:00 +0530

    Delete Employee Management System directory

--- Employee Management System/src/main/java/com/projects/ems/application/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projects.ems.application.repository;
-
-import com.projects.ems.application.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
